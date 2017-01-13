6d5e5301227d72d745173911194dd279acfe3be2
Jhen <developer@jhen.me>
2017-01-14 01:55:49 +0800

    Check `<app/>` and `<updatecheck/>` status prop for isUpdated

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,6 @@
 import electron, { remote } from 'electron';
 import { parseString } from 'xml2js';
 import path from 'path';
-import semver from 'semver';
 import request from 'request';
 
 
@@ -10,17 +9,20 @@ export const getPath = () => {
   return path.resolve(`${savePath}/extensions`);
 };
 
-export const isUpdated = chromeStoreID => new Promise((resolve, reject) => {
+export const isUpdated = (chromeStoreID, currentVersion) => new Promise((resolve, reject) => {
   request.get(
       `https://clients2.google.com/service/update2/crx?x=id%3D${chromeStoreID}%26uc&prodversion=32`,
       (error, response, body) => {
         if (!error && response.statusCode === 200) {
           parseString(body, (err, result) => {
-            const newestVersion = result.gupdate.app[0].updatecheck[0].$.version;
-            const currentVersion = require(path.resolve(getPath(), chromeStoreID, 'manifest.json')).version; // eslint-disable-line global-require
-            console.log(currentVersion);
-            if (semver.gt(newestVersion, currentVersion)) resolve(false);
-            else resolve(true);
+            const app = result.gupdate.app[0].$;
+            if (app.status !== 'ok') return resolve(true);
+
+            const { status, version: newestVersion } = result.gupdate.app[0].updatecheck[0].$;
+            if (status !== 'ok') return resolve(true);
+
+            if (newestVersion !== currentVersion) return resolve(false);
+            return resolve(true);
           });
         } else {
           reject(`Failed to check current version of ${chromeStoreID}.`);
